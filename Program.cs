7f5f9dc47f579c5527f7142bbf620d14065da3bb
NavyaMaddineni <navyamaddineni911@gmail.com>
2025-02-26 10:12:29 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+
+namespace Hello
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Debug.WriteLine("Hello, World!");
+            Console.ReadLine();
+
+            int x;
+            x = 2;
+        }
+    }
+}
